5b950131148725ca7011cb64b118f91d0cf6fdd7
Andrey Astashov <mvc.aaa@gmail.com>
2017-08-11 18:10:39 +0300

    asdf

--- scripts/teamcity/prepare.sh
+++ scripts/teamcity/prepare.sh
@@ -5,3 +5,12 @@ printenv
 
 cd ${COMPOSER_HOME}
 composer --dev --verbose --optimize-autoloader install
+vendor/bin/phinx --version
+
+mysql --host="${MYSQL_HOST}" --user="${MYSQL_USER}" --password="${MYSQL_ROOT_PASSWORD}" -e exit 2>/dev/null
+dbstatus=`echo $?`
+
+if [ $dbstatus -ne 0 ]; then
+  echo "MySQL connection problems, exiting"
+  exit $dbstatus
+fi
