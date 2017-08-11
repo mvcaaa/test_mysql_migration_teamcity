ddee17b09d0b0d1f9b69b9be6047593dffcde262
Andrey Astashov <mvc.aaa@gmail.com>
2017-08-11 18:31:06 +0300

    asd

--- scripts/teamcity/prepare.sh
+++ scripts/teamcity/prepare.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export COMPOSER_HOME="$(pwd)/lib"
-printenv
+# printenv
 
 cd ${COMPOSER_HOME}
-composer --dev --verbose --optimize-autoloader install
+composer --dev --optimize-autoloader install
 vendor/bin/phinx --version
 
 mysql --host="${MYSQL_HOST}" --user="${MYSQL_USER}" --password="${MYSQL_ROOT_PASSWORD}" -e exit 2>/dev/null
