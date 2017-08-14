9fa791048716ad8fac1e077f72b43ee5433aa92b
Andrey Astashov <mvc.aaa@gmail.com>
2017-08-14 20:38:41 +0300

    Full cycle

--- scripts/teamcity/prepare.sh
+++ scripts/teamcity/prepare.sh
@@ -4,7 +4,7 @@ export COMPOSER_HOME="$(pwd)/lib"
 # printenv
 
 cd ${COMPOSER_HOME}
-composer --dev --optimize-autoloader install
+composer install
 vendor/bin/phinx --version
 
 mysql --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" -e exit 2>/dev/null
