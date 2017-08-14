30bbd6af7c920e9696c621ecd4e91bcc26a64bac
Andrey Astashov <mvc.aaa@gmail.com>
2017-08-14 20:23:38 +0300

    Changes

--- scripts/teamcity/init.sh
+++ scripts/teamcity/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-mysql --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" -e "DROP DATABASE nexus_base; CREATE DATABASE nexus_base;"
+mysql --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" -e "DROP DATABASE nexus_base;"
+mysql --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" -e "CREATE DATABASE nexus_base;"
 mysql --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" nexus_base < nexus_sql/nexus_base.sql
 
 mysqladmin --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" create "${PHINX_NEXUS_DB_NAME}"
