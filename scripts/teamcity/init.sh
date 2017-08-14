98e802fcc072cd2d0c17385bfc3cf4c3b9a2d0f3
Andrey Astashov <mvc.aaa@gmail.com>
2017-08-14 20:03:42 +0300

    Changes

--- scripts/teamcity/init.sh
+++ scripts/teamcity/init.sh
@@ -2,4 +2,6 @@
 
 mysql --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" create "${PHINX_NEXUS_DB_NAME}"
 
-vendor/bin/phinx status -c phinx_template.php 
+cd "$(pwd)/lib"
+
+vendor/bin/phinx status -c phinx_template.php
