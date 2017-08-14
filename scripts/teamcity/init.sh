cbb2dde137c67265db60f0cff49584055764e38b
Andrey Astashov <mvc.aaa@gmail.com>
2017-08-14 19:40:18 +0300

    Changes

---
+++ scripts/teamcity/init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mysql --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" create 
+${PHINX_NEXUS_DB_NAME}
+
+vendor/bin/phinx status -c phinx_template.php 
--- scripts/teamcity/prepare.sh
+++ scripts/teamcity/prepare.sh
@@ -7,8 +7,6 @@ cd ${COMPOSER_HOME}
 composer --dev --optimize-autoloader install
 vendor/bin/phinx --version
 
-vendor/bin/phinx status -c phinx_template.php -e docker
-
 mysql --host="${PHINX_NEXUS_DB_HOST}" --user="${PHINX_NEXUS_DB_USER}" --password="${PHINX_NEXUS_DB_PASSWORD}" -e exit 2>/dev/null
 dbstatus=`echo $?`
 
